60f60a58fa5155c6832fca19a0ef23b0d2d5a104
phil-kahrl <phil_kahrl@hotmail.com>
2020-10-27 14:22:20 -0700

    Fleshing out the example. Added README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# discriminator-demo
+
+Examples on how to use [arccore.discriminator](https://encapsule.io/docs/ARCcore/discriminator).
+
+Discriminator provides a way to build a router function in JavaScript.
+
+A router function is a function that routes to other functions based on the shape of the request object
+passed to the function as a single argument.
+
+Discriminator can be used to build extensible API's in JavaScript that can be used for a variety of applications.
+
+## System Requirements
+
+[Yarn package manager](https://yarnpkg.com/)
+
+[Node.js](https://nodejs.org/)
+
+## Usage
+
+`yarn install` will install dependencies.
+
+`node index.js` to run the example code.
+
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let factoryResponse= arccore.filter.create({
     operationID: "VzjSyFMoQlOhjeUhYx8mUg",
     operationName: "Filter 1",
     operationDescription: "Filter 1",
+    outputFilterSpec: {____accept: "jsString"},
     inputFilterSpec: {
         ____types: "jsObject",
         A: {
@@ -11,7 +12,7 @@ let factoryResponse= arccore.filter.create({
         }
     },
     bodyFunction: (request_) => {
-        return {result: undefined}
+        return {result: "Hello from filter1"};
     }
 });
 
@@ -23,6 +24,7 @@ factoryResponse= arccore.filter.create({
     operationID: "hm8AwWv2Q7aShmcOWVUtFQ",
     operationName: "Filter 2",
     operationDescription: "Filter 2",
+    outputFilterSpec: {____accept: "jsString"},
     inputFilterSpec: {
         ____types: "jsObject",
         B: {
@@ -30,7 +32,7 @@ factoryResponse= arccore.filter.create({
         }
     },
     bodyFunction: (request_) => {
-        return {result: undefined}
+        return {result: "Hello from filter2"};
     }
 });
 
@@ -38,7 +40,7 @@ if (factoryResponse.error) throw new Error(factoryResponse.error);
 
 let filter2 = factoryResponse.result;
 
-const discriminatorFactory = arccore.discriminator.create({
+let discriminatorFactoryResponse = arccore.discriminator.create({
     filters: [
         filter1,
         filter2
@@ -48,15 +50,13 @@ const discriminatorFactory = arccore.discriminator.create({
     }
 });
 
-if (discriminatorFactory.error) throw new Error (discriminatorFactory.error);
-
-const discriminator = discriminatorFactory.result;
+const discriminator = discriminatorFactoryResponse.result;
 
 let response = discriminator.request({A: "argh"});
-console.log(response); //{ error: null, result: undefined }
+console.log(response); //{ error: null, result: 'Hello from filter1' }
 
 response = discriminator.request({B: "baz"});
-console.log(response); //{ error: null, result: undefined }
+console.log(response); //{ error: null, result: 'Hello from filter2' }
 
 response = discriminator.request({C: "cat"});
 console.log(response);
@@ -66,13 +66,56 @@ console.log(response);
     Discriminator constructor will return an error if number of filters < 1
 **/
 
-//TODO
+discriminatorFactoryResponse = arccore.discriminator.create({
+    filters: [
+        filter2
+    ],
+    options: {
+        action: "routeRequest" //____inValueSet ["getFilterID","getFilter","routeRequest"]
+    }
+});
+
+console.log(discriminatorFactoryResponse);
+//{error: 'Filter [5A8uDJunQUm1w-HcBPQ6Gw::Request Discriminator Filter Factory] failed while performing main operation. Invalid request. You must specify an array of two or more Filter objects to construct a Discriminator Filter.',result: null}
 
 /**
     Discriminator returns an error if filter specs over lap
 **/
 
-//TODO
+factoryResponse= arccore.filter.create({
+    operationID: "xL2qDIobSMenfUu8cu6ayw",
+    operationName: "Filter 3",
+    operationDescription: "Filter 3",
+    inputFilterSpec: {
+        ____types: "jsObject",
+        A: {
+            ____types: ["jsString", "jsObject"],
+            C: {
+                ____accept: "jsObject",
+                ____defaultValue: {}
+            }
+        }
+    },
+    bodyFunction: (request_) => {
+        return {result: undefined}
+    }
+});
+
+let filter3 = factoryResponse.result;
+
+discriminatorFactoryResponse = arccore.discriminator.create({
+    filters: [
+        filter1,
+        filter2,
+        filter3
+    ],
+    options: {
+        action: "routeRequest"
+    }
+});
+
+console.log(discriminatorFactoryResponse);
+//{error: "Filter [5A8uDJunQUm1w-HcBPQ6Gw::Request Discriminator Filter Factory] failed while performing main operation. Filters [VzjSyFMoQlOhjeUhYx8mUg, xL2qDIobSMenfUu8cu6ayw] overlap ambiguously at filter spec node 'request(jsObject).A(jsString)'.",result: null}
 
 
 
